a6d9c55bea12b115d617415b0a4bd634c2e3b6c1
Piotr Hałas <piotr@halas.net.pl>
2019-10-29 18:50:41 +0100

    remove app name form footer

--- templates/_partial/footer.php
+++ templates/_partial/footer.php
@@ -1,6 +1,6 @@
 <footer class="bg-dark">
     <div class="container">
-        <p class="m-0 text-center text-white"><a class="text-white" href="https://crazy-goat.com">Crazy Goat Software</a>
-            &copy; <?= $this->trans('app.name').' '.date('Y') ?> </p>
+        <p class="m-0 text-center text-white"><a class="text-white" href="https://crazy-goat.com">Crazy Goat
+                Software</a>&copy; <?= date('Y') ?> </p>
     </div>
 </footer>
